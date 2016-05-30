47c4d8a20d2def8f57692f32100f093a59072bca
winfredselwyn <winfredselwyn@gmail.com>
2016-05-30 04:34:15 +0000

    Completed Ruby Koans

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,33 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  # :equilateral  if all sides are equal
+  # if a == b && a == c
+  #   :equilateral
+  # :isosceles if exactly 2 sides are equal
+  # elsif a == b || b == c || a == c
+  #   :isosceles
+  # :scalene if no sides are equal
+  # elsif a != b || b != c || c != a 
+  #   :scalene
+  # end
+  
+  if a <= 0 ||  b <= 0 || c <= 0
+    raise TriangleError
+  end 
+  
+  if a + b <= c  || a + c <= b ||  b + c <= a
+    raise TriangleError
+  end 
+  
+  if a == b && a == c 
+    :equilateral
+  elsif a == b || b == c || a == c
+    :isosceles   
+  else a != b || b != c || c != a 
+    :scalene     
+  end
+  
 end
 
 # Error class used in part 2.  No need to change this code.
